faf214423125e4ef8be8e6657b3364094114c6c3
Jahidfahad <jahidfahad017@gmail.com>
2020-05-04 22:18:43 +0600

    added api and checked it

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import CountryPicker from './components/CountryPicker/CountryPicker';
+import Cards from './components/Cards/Cards';
+import Chart from './components/Chart/Chart'
+import {fetchData} from './components/api'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  async componentDidMount(){
+    const fetchData = await fetchData();
+    console.log(fetchData)
+  }
+
+  render() {
+    return (
+      <div>
+        <Cards></Cards>
+        <CountryPicker></CountryPicker>
+        <Chart></Chart>
+      </div>
+    );
+  }
 }
 
 export default App;
